e17bb734e9ad9c7e079bfc8b355e7d1eb3df785b
jordan-git-lewis <jlgitrepo@gmail.com>
2023-06-27 09:26:32 -0700

    refactored duplicate code

--- Psst Dataset/preprocess.py
+++ Psst Dataset/preprocess.py
@@ -20,58 +20,26 @@ def convert_tsv_data_to_csv(dataset):
     Args: dataset - The dataset to read and convert to csv. 
     """
     try:
-        if dataset == test_directory:
-
-            working_directory = read_directory + test_directory
-            file_path = os.path.join(write_directory, test_directory + "_" + file_name)
-            
-            # Read respective tsv file and the write to test_utterances.csv
-            if not os.path.isfile(file_path):
-                with open (working_directory + "/" + tsv_file_name, 'r') as myfile:
-                    with open(file_path, 'w') as csv_file:
-                        for line in myfile:
-
-                            fileContent = re.sub("\t", ",", line)
-                            csv_file.write(fileContent)
-            
-            # modify columns in the data set    
-            psstData = load_dataset('csv', data_files= write_directory + "/" + test_directory + "_" + file_name)
-            psstData = psstData.remove_columns(remove_columns)
-
-        elif dataset == train_directory:
-
-            working_directory = read_directory + train_directory
-            file_path = os.path.join(write_directory, train_directory + "_" + file_name)
-            
-            if not os.path.isfile(file_path):
-                with open (working_directory + "/" + tsv_file_name, 'r') as myfile:
-                    with open(file_path, 'w') as csv_file:
-                        for line in myfile:
-
-                            fileContent = re.sub("\t", ",", line)
-                            csv_file.write(fileContent)
-            
-            psstData = load_dataset('csv', data_files= write_directory + "/" + train_directory + "_" + file_name)
-            psstData = psstData.remove_columns(remove_columns)
         
-        elif dataset == valid_directory:
+        working_directory = read_directory + dataset
+        file_path = os.path.join(write_directory, dataset + "_" + file_name)
+        
+        if not os.path.isfile(file_path):
+            # Read respective tsv file and the write to test_utterances.csv
+            with open (working_directory + "/" + tsv_file_name, 'r') as myfile:
+                with open(file_path, 'w') as csv_file:
+                    for line in myfile:
 
-            working_directory = read_directory + valid_directory
-            file_path = os.path.join(write_directory, valid_directory + "_" + file_name)
-
-            if not os.path.isfile(file_path):
-                with open (working_directory + "/" + tsv_file_name, 'r') as myfile:
-                    with open(file_path, 'w') as csv_file:
-                        for line in myfile:
-
-                            fileContent = re.sub("\t", ",", line)
-                            csv_file.write(fileContent)
+                        fileContent = re.sub("\t", ",", line)
+                        csv_file.write(fileContent)
             
-            psstData = load_dataset('csv', data_files= write_directory + "/" + valid_directory + "_" + file_name)
-            psstData = psstData.remove_columns(remove_columns)
+        # modify columns in the data set    
+        psstData = load_dataset('csv', data_files= write_directory + "/" + dataset + "_" + file_name)
+        psstData = psstData.remove_columns(remove_columns)
 
         print(psstData)
         return psstData
+    
     except FileNotFoundError as e:
         print("Error: ", e)
 
